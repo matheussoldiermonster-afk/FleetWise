62c2fb8a32fbfa2df52cbbca2d4f8d4f3b5a40cb
Matheus Borges <matheussoldiermonster@gmail.com>
2026-08-19 16:52:47 -0300

    Adiciona script para criar usuario inicial

---
+++ backend/create-admin.js
@@ -0,0 +1,31 @@
+const prisma = require("./src/config/prisma");
+const bcrypt = require("bcrypt");
+
+async function main() {
+  const password = 123456;
+
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const company = await prisma.company.create({
+    data: {
+      name: "FleetWise Demo",
+      email: "matt@fleetwise.com",
+    },
+  });
+
+  const user = await prisma.user.create({
+    data: {
+      name: "Matheus",
+      email: "SEU_EMAIL_AQUI",
+      password: passwordHash,
+      companyId: company.id,
+    },
+  });
+
+  console.log("Empresa criada:", company.id);
+  console.log("Usuário criado:", user.email);
+}
+
+main()
+  .catch(console.error)
+  .finally(() => prisma.$disconnect());
